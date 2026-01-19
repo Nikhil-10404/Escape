36d42112c4cfcc0c931b8096cb795fb0f6710b6b
Nikhil <yognik456@gmail.com>
2026-01-19 19:54:54 +0530

    initial commit

--- app/src/config/api.ts
+++ app/src/config/api.ts
@@ -2,6 +2,6 @@ import Constants from "expo-constants";
 
 const baseUrl =
   (Constants.expoConfig?.extra?.API_BASE_URL as string) ||
-  "http://10.254.19.198:5000"; // fallback (optional)
+  "http://192.168.62.198:5000"; // fallback (optional)
 
 export const API = `${baseUrl}/api/auth`;
